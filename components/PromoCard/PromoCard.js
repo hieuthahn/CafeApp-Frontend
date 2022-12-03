c5224182014138394ce4e616cbbb06e3d3232c86
hiennd <hiennd.codetot@gmail.com>
2022-12-03 14:55:51 +0700

    UI promos

--- components/PromoCard/PromoCard.js
+++ components/PromoCard/PromoCard.js
@@ -1,30 +1,21 @@
 import Image from 'next/image'
-import Link from 'next/link'
-import { Modal } from 'antd'
-import { useState } from 'react'
 
 const PromoCard = (props) => {
     const { imageUrl, title, name, address } = props
-    const [openModal, setOpenModal] = useState(false)
 
     return (
         <>
-            <div
-                className="w-full rounded-xl shadow-md overflow-hidden"
-                onClick={() => setOpenModal(true)}
-            >
+            <div className="w-full rounded-xl shadow-md overflow-hidden">
                 <div className="relative w-full h-[250px] xl:h-[300px]">
-                    <Link href={''}>
-                        <a>
-                            <Image
-                                alt="cafe-app"
-                                layout="fill"
-                                objectFit="cover"
-                                src={imageUrl}
-                                className="object-cover object-center hover:scale-105 transition ease-in duration-500"
-                            />
-                        </a>
-                    </Link>
+                    <a>
+                        <Image
+                            alt="cafe-app"
+                            layout="fill"
+                            objectFit="cover"
+                            src={imageUrl}
+                            className="object-cover object-center hover:scale-105 transition ease-in duration-500"
+                        />
+                    </a>
                 </div>
                 <div className="p-4 pt-2 bg-white">
                     <p className="border-b pb-3 text-base text-rose-500 font-bold truncate">
@@ -38,47 +29,6 @@ const PromoCard = (props) => {
                     </div>
                 </div>
             </div>
-            <Modal
-                title={
-                    <h3 className="text-xl font-bold text-center">
-                        {'Chi tiết khuyến mãi'}
-                    </h3>
-                }
-                open={openModal}
-                onCancel={() => setOpenModal(false)}
-                centered
-                footer={false}
-            >
-                <div>
-                    <h2 className="font-bold text-xl">{name}</h2>
-                    <p className="text-rose-500 font-bold text-xl mt-8">
-                        {title}
-                    </p>
-                    <p className="mt-4">{`Địa chỉ: ${address}`}</p>
-                    <div className="flex">
-                        <div className="relative h-[120px] w-[120px] mt-4 mr-4">
-                            <Image
-                                alt="cafe-app"
-                                layout="fill"
-                                objectFit="cover"
-                                src={
-                                    'https://ik.imagekit.io/reviewcafe/promo/122878652_3548009975277797_6943584230145280813_n_1MyxdRTK9j.jpg?tr=w-400,q-80'
-                                }
-                                className="object-cover object-center hover:scale-105 transition ease-in duration-500"
-                            />
-                        </div>
-                        <div className="relative h-[120px] w-[120px] mt-4">
-                            <Image
-                                alt="cafe-app"
-                                layout="fill"
-                                objectFit="cover"
-                                src={imageUrl}
-                                className="object-cover object-center hover:scale-105 transition ease-in duration-500"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </Modal>
         </>
     )
 }
--- pages/promo/index.js
+++ pages/promo/index.js
@@ -2,15 +2,16 @@ import PromoCard from 'components/PromoCard'
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Modal } from 'antd'
 import { getPromos } from 'lib/services/promos'
-
-const array = [1, 2, 3, 4, 5, 6, 7, 8]
 
 const Promo = () => {
     const [promos, setPromos] = useState([])
+    const [promo, setPromo] = useState({})
+    const [openModal, setOpenModal] = useState(false)
     const [params, setParams] = useState({
         paged: 1,
-        pageSize: 10
+        pageSize: 10,
     })
     const getListPromos = async () => {
         const res = await getPromos(params.paged, params.pageSize)
@@ -19,10 +20,14 @@ const Promo = () => {
         }
     }
 
-useEffect(() => {
-    getListPromos()
-}, [])
-console.log(promos)
+    useEffect(() => {
+        getListPromos()
+    }, [])
+
+    const handleOpenModal = (data) => {
+        setPromo(data)
+        setOpenModal(true)
+    }
     return (
         <div className="mb-8">
             <div className="h-[220px] bg-[linear-gradient(180deg,#ffb8b8,#f3f4f6)] hidden lg:block">
@@ -42,34 +47,76 @@ console.log(promos)
                         </div>
 
                         <div className="relative h-[160px] w-[270px]">
-                            <Link href={''}>
-                                <a>
-                                    <Image
-                                        alt="cafe-app"
-                                        layout="fill"
-                                        objectFit="cover"
-                                        src={'https://toidicafe.vn/images/discount.svg'}
-                                        className="object-cover object-center hover:scale-105 transition ease-in duration-500"
-                                    />
-                                </a>
-                            </Link>
+                            <a>
+                                <Image
+                                    alt="cafe-app"
+                                    layout="fill"
+                                    objectFit="cover"
+                                    src={
+                                        'https://toidicafe.vn/images/discount.svg'
+                                    }
+                                    className="object-cover object-center hover:scale-105 transition ease-in duration-500"
+                                />
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="container mx-auto px-2 mt-8">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                    {array.map((item, index) => {
+                    {promos.map((item, index) => {
                         return (
-                            <PromoCard
+                            <div
                                 key={index}
-                                imageUrl="https://ik.imagekit.io/reviewcafe/promo/217415672_4341774135903397_5329500902239270033_n_5XoTA_iQeo.jpg?tr=w-400,q-80"
-                                title="Order thả ga - Hoàn ngay nửa giá"
-                                name="MONO Coffee Lab"
-                                address="03 Hồ Xuân Hương, Hai Bà Trưng, Hà Nội"
-                            />
+                                onClick={() => {
+                                    handleOpenModal(item)
+                                }}
+                            >
+                                <PromoCard
+                                    imageUrl={
+                                        item.images && item.images[0]?.url
+                                    }
+                                    title={item.title}
+                                    name={item.place.name}
+                                    address={item.place.address.specific}
+                                />
+                            </div>
                         )
                     })}
+
+                    <Modal
+                        title={
+                            <h3 className="text-xl font-bold text-center">
+                                {'Chi tiết khuyến mãi'}
+                            </h3>
+                        }
+                        open={openModal}
+                        onCancel={() => setOpenModal(false)}
+                        centered
+                        footer={false}
+                    >
+                        <div>
+                            <h2 className="font-bold text-xl">{promo.name}</h2>
+                            <p className="text-rose-500 font-bold text-xl mt-8">
+                                {promo.title}
+                            </p>
+                            <p className="mt-4">{promo.description}</p>
+                            <p className="mt-4 font-bold">{`Địa chỉ: ${promo.place.address.specific}`}</p>
+                            <div className="flex">
+                                <div className="relative h-[120px] w-[120px] mt-4">
+                                    <Image
+                                        alt="cafe-app"
+                                        layout="fill"
+                                        objectFit="cover"
+                                        src={
+                                            promo.images && promo.images[0].url
+                                        }
+                                        className="object-cover object-center hover:scale-105 transition ease-in duration-500"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
